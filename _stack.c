e861f6de94d1b8bd35a6ff02eee5afa7f18b0523
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2023-10-21 23:36:50 +0100

    Update _stack.c

--- _stack.c
+++ _stack.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 /**
  * print_stack - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
+ * @head: The stack head
+ * @counter: The line_number
+ * Return: no return value
 */
 void print_stack(stack_t **head, unsigned int counter)
 {
